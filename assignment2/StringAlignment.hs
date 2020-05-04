242da564efab238e2427deee4b1a19ccefea2fba
Clemens Christierson <christierson.c@gmail.com>
2020-05-04 23:04:13 +0200

    attachHeads funkar, fattar inte similarityScore

--- assignment2/StringAlignment.hs
+++ assignment2/StringAlignment.hs
@@ -1,1 +1,8 @@
-StringAlignment.hs
+module StringAlignment where
+
+    similarityScore :: String -> String -> Int
+    similarityScore s1 s2 = 0
+
+    attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
+    attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
+    
