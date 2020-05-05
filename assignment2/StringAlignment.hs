589bbc91a593248f26993e113e858a528d4cb5cd
Clemens Christierson <christierson.c@gmail.com>
2020-05-05 15:55:59 +0200

    fixade några errors

--- assignment2/StringAlignment.hs
+++ assignment2/StringAlignment.hs
@@ -1,20 +1,17 @@
 module StringAlignment where
 
-	scoreMatch = 0
-	scoreMismatch = -1
-	scoreSpace = -1
+    scoreMatch = 0
+    scoreMismatch = -1
+    scoreSpace = -1
 
-	-- STRÄNGAR --
+    -- STRÄNGAR --
 
-	string1 = "writers"
-	string2 = "vintner"
-	string3 = "aferouciousmonadatemyhamster"
-	string4 = "functionalprogrammingrules"
-	string5 = "bananrepubliksinvasionsarmestabsadjutant"
-	string6 = "kontrabasfiolfodralmakarmästarlärling"
-
-    similarityScore :: String -> String -> Int
-    similarityScore s1 s2 = 0
+    string1 = "writers"
+    string2 = "vintner"
+    string3 = "aferouciousmonadatemyhamster"
+    string4 = "functionalprogrammingrules"
+    string5 = "bananrepubliksinvasionsarmestabsadjutant"
+    string6 = "kontrabasfiolfodralmakarmästarlärling"
 
     attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
     attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
@@ -22,21 +19,21 @@ module StringAlignment where
 
     -- ANVÄNDER SAMMA LOOKUP-TABELL SOM I MCS-PROBLEMET --
     similarityScore :: String -> String -> Int
-    similarityScore xs ys = getIndex (length xs) (length ys)
-      where
-    	getEntry i j = table !! i !! j
+    similarityScore xs ys = getEntry (length xs) (length ys)
+        where
+            getEntry i j = table !! i !! j
 
-    	table :: [[Int]]
-    	table = [[entry i j | j <- [0..]] | i <- [0..]]
+            table :: [[Int]]
+            table = [[entry i j | j <- [0..]] | i <- [0..]]
 
-    	entry :: Int -> Int -> Int
-    	entry _ 0 = 0
-    	entry 0 _ = 0
-    	entry i j
-    	  | x = y = 1 + getEntry (i - 1) (j - 1)
-    	  | otherwise = max (getEntry i (j - 1)) (getEntry (i - 1) j)
-    	  where
-    	  	x = xs !! (i - 1)
-    	  	y = ys !! (j - 1)
+            entry :: Int -> Int -> Int
+            entry _ 0 = 0
+            entry 0 _ = 0
+            entry i j
+                | x == y = 1 + getEntry (i - 1) (j - 1)
+                | otherwise = max (getEntry i (j - 1)) (getEntry (i - 1) j)
+                where
+                    x = xs !! (i - 1)
+                    y = ys !! (j - 1)
 
 
